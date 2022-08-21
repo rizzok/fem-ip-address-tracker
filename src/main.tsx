3d4748668d2ca939eab662afc4d594714a6f06d4
rizzok <suncode13@gmail.com>
2022-08-21 21:47:25 +0200

    Add Redux

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import 'minireset.css'
 import './style.css.ts'
+import { Provider } from 'react-redux'
+import store from './store'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 )
